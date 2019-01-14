b434345631577fbba17f28f77639cd9a0bf0437a
thurley998 <46353958+thurley998@users.noreply.github.com>
2019-01-14 12:51:27 -0700

    fix

--- home.js
+++ home.js
@@ -1,10 +1,10 @@
-unction myFunction() {
+function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropbutton')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
