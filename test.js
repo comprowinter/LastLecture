5217a8ad04615feebb99b8ef08ba6eb9ebe88be0
thurley998 <46353958+thurley998@users.noreply.github.com>
2019-01-15 13:02:43 -0700

    ok

---
+++ test.js
@@ -0,0 +1,10 @@
+("#essayRow").mouseover(function() {
+    ('#essaybox1').(".margin");
+    ('#essaybox2').attr(".margin");
+    ('#essaybox3').attr(".margin");
+    ('#essaybox4').attr(".margin");
+}).mouseout(function() {
+    ('#div1').removeClass('hover');
+    ('#div2').removeClass('hover');    
+    ('#div3').removeClass('hover');
+});
