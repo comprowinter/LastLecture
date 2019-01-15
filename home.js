3f221b62f6424f27def3d832598be0a3844bd79c
thurley998 <46353958+thurley998@users.noreply.github.com>
2019-01-15 12:04:27 -0700

    update

--- home.js
+++ home.js
@@ -5,7 +5,25 @@ function myFunction(id) {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbutton')) {
-    var dropdowns = document.getElementsByClassName(".dropdown-content");
+    var dropdowns = document.getElementsByClassName(".dropdown-content1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function myFunction(id) {
+  document.getElementById("mydropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbutton')) {
+    var dropdowns = document.getElementsByClassName(".dropdown-content1");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
