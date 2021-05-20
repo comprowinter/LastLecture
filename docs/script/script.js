a14a4424f15d281e65c43a0a73ad4996030c79d9
Tate <wondertate@gmail.com>
2021-05-20 15:33:10 -0600

    All page major overhaul

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,5 +1,5 @@
 function LoadPage(name) {
-    window.open('./pdf/'+name+'.pdf', '_self');
+    window.open('/docs/pdf/'+name+'.pdf', '_self');
 }
 
 function selectYear(year) {
@@ -11,6 +11,28 @@ function selectYear(year) {
         }
         else {
             lectures[i].style.display = "none";
+        }
+    }
+}
+
+function searchTable() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("lectures-search-bar");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("lectures");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
     }
 }
